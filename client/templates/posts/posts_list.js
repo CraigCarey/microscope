63b717c4f1466408ed0982534b501d2e1db11f0f
User <user@Users-iMac.home>
2015-06-05 10:07:53 +0100

    Sort posts by submission timestamp

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -6,6 +6,7 @@ Template.postsList.helpers({
         // find() returns a cursor, which is a reactive data source
         // When we tell Spacebars to iterate over the Posts.find() cursor, it knows to observe that cursor for changes
         // We can use fetch() on that cursor to transform it into an array
-        return Posts.find();
+        // sort all ({}) by date submitted, most recent first (-1)
+        return Posts.find({}, {sort: {submitted: -1}});
     }
 });
