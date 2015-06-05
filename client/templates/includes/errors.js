a87ee8949797c9052c7cc57b37abe888ec3cfb2d
User <user@Users-iMac.home>
2015-06-05 11:50:24 +0100

    Clear errors after 3 seconds

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,13 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+// remove errors after they've been displayed
+Template.error.onRendered(function() {
+    var error = this.data;
+
+    // callback fx is executed after timeOut
+    Meteor.setTimeout(function () {
+        Errors.remove(error._id);
+    }, 3000);
+});
