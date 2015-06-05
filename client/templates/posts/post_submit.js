5929dbe402740c8a8d5d1dffcb12780a76a397d0
User <user@Users-iMac.home>
2015-06-05 10:42:27 +0100

    Chapter 7.5 - Demo latency compensation

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -30,7 +30,10 @@ Template.postSubmit.events({
             }
 
             // redirect browser to new post page if postInsert is successful
-            Router.go('postPage', {_id: result._id});
+            //Router.go('postPage', {_id: result._id});
         });
+
+        // test code for demonstrating latency compensation
+        Router.go('postsList');
     }
 });
