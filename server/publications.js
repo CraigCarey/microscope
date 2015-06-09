2421f1cae4a3d5753ac92d132e548a2dddde75ba
User <user@Users-iMac.home>
2015-06-09 13:51:14 +0100

    Use a single post subscription to ensure that we can always see the right post

--- server/publications.js
+++ server/publications.js
@@ -14,6 +14,12 @@ Meteor.publish('posts', function(options) {
     return Posts.find({}, options);
 });
 
+// overcomes not found error introduced after implementing pagination
+Meteor.publish('singlePost', function(id) {
+    check(id, String);
+    return Posts.find(id);
+});
+
 Meteor.publish('comments', function(postId) {
     check(postId, String);
     return Comments.find({postId: postId});
